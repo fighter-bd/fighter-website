1c64719a1fd14a349b875166c573da4bc3781ba9
fighter-bd <bornosarker54@gmail.com>
2026-06-21 15:58:28 +0600

    Add files via upload
    Signed-off-by: fighter-bd <bornosarker54@gmail.com>

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "fighter-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./fighter-cricket-logo.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch (offline support)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
